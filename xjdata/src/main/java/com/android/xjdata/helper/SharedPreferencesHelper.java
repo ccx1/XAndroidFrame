9b3d16fe0800f04ed2693318e22d0379bdba8d55
ccx <ci250454344@163.com>
2018-11-22 16:47:28 +0800

    更新助手

--- xjdata/src/main/java/com/android/xjdata/helper/SharedPreferencesHelper.java
+++ xjdata/src/main/java/com/android/xjdata/helper/SharedPreferencesHelper.java
@@ -1,12 +1,17 @@
 package com.android.xjdata.helper;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import com.android.xjdata.common.Reflex;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+
 
 /**
  * @author ccx
@@ -14,10 +19,11 @@ import java.util.Set;
  */
 public class SharedPreferencesHelper {
 
+    @SuppressLint("StaticFieldLeak")
     private static SharedPreferencesHelper                        sSharedPreferences;
-    private static android.content.SharedPreferences              sXj_config;
+    private static SharedPreferences              sXj_config;
     private static Context                                        sApplicationContext;
-    private        Map<String, android.content.SharedPreferences> mPreferencesMap = new HashMap<>();
+    private        Map<String, SharedPreferences> mPreferencesMap = new HashMap<>();
 
     private SharedPreferencesHelper() {
     }
@@ -40,7 +46,7 @@ public class SharedPreferencesHelper {
 
 
     public void put(String key, Object object) {
-        android.content.SharedPreferences.Editor edit = edit();
+        SharedPreferences.Editor edit = edit();
         if (object instanceof String) {
             edit.putString(key, ((String) object));
         } else if (object instanceof Boolean) {
@@ -56,8 +62,7 @@ public class SharedPreferencesHelper {
         } else {
             edit.putString(key, object.toString());
         }
-        edit.apply();
-
+        SharedPreferencesCompat.apply(edit);
     }
 
     public Object get(String key) {
@@ -65,7 +70,7 @@ public class SharedPreferencesHelper {
         return all.get(key);
     }
 
-    private android.content.SharedPreferences.Editor edit() {
+    private SharedPreferences.Editor edit() {
         return sXj_config.edit();
     }
 
@@ -75,7 +80,7 @@ public class SharedPreferencesHelper {
     }
 
     public String getString(String name, String key, String defValue) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
@@ -85,12 +90,13 @@ public class SharedPreferencesHelper {
     }
 
     public void putString(String name, String key, String value) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
         }
-        sharedPreferences.edit().putString(key, value).apply();
+        SharedPreferences.Editor editor = sharedPreferences.edit().putString(key, value);
+        SharedPreferencesCompat.apply(editor);
     }
 
     public long getLong(String name, String key) {
@@ -98,7 +104,7 @@ public class SharedPreferencesHelper {
     }
 
     public long getLong(String name, String key, long defValue) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
@@ -108,12 +114,13 @@ public class SharedPreferencesHelper {
     }
 
     public void putLong(String name, String key, long value) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
         }
-        sharedPreferences.edit().putLong(key, value).apply();
+        SharedPreferences.Editor editor = sharedPreferences.edit().putLong(key, value);
+        SharedPreferencesCompat.apply(editor);
     }
 
     public float getFloat(String name, String key) {
@@ -121,7 +128,7 @@ public class SharedPreferencesHelper {
     }
 
     public float getFloat(String name, String key, float defValue) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
@@ -131,12 +138,13 @@ public class SharedPreferencesHelper {
     }
 
     public void putFloat(String name, String key, float value) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
         }
-        sharedPreferences.edit().putFloat(key, value).apply();
+        SharedPreferences.Editor editor = sharedPreferences.edit().putFloat(key, value);
+        SharedPreferencesCompat.apply(editor);
     }
 
     public int getInt(String name, String key) {
@@ -144,7 +152,7 @@ public class SharedPreferencesHelper {
     }
 
     public int getInt(String name, String key, int defValue) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
@@ -154,12 +162,13 @@ public class SharedPreferencesHelper {
     }
 
     public void putInt(String name, String key, int value) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
         }
-        sharedPreferences.edit().putInt(key, value).apply();
+        SharedPreferences.Editor editor = sharedPreferences.edit().putInt(key, value);
+        SharedPreferencesCompat.apply(editor);
     }
 
     public Set<String> getSet(String name, String key) {
@@ -167,7 +176,7 @@ public class SharedPreferencesHelper {
     }
 
     public Set<String> getSet(String name, String key, Set<String> defValue) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
@@ -177,12 +186,13 @@ public class SharedPreferencesHelper {
     }
 
     public void putSet(String name, String key, Set<String> value) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
         }
-        sharedPreferences.edit().putStringSet(key, value).apply();
+        SharedPreferences.Editor editor = sharedPreferences.edit().putStringSet(key, value);
+        SharedPreferencesCompat.apply(editor);
     }
 
     public boolean getBoolean(String name, String key) {
@@ -190,7 +200,7 @@ public class SharedPreferencesHelper {
     }
 
     public boolean getBoolean(String name, String key, boolean defValue) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
@@ -200,12 +210,70 @@ public class SharedPreferencesHelper {
     }
 
     public void putBoolean(String name, String key, boolean value) {
-        android.content.SharedPreferences sharedPreferences = mPreferencesMap.get(name);
+        SharedPreferences sharedPreferences = mPreferencesMap.get(name);
         if (sharedPreferences == null) {
             sharedPreferences = sApplicationContext.getSharedPreferences(name, Context.MODE_PRIVATE);
             mPreferencesMap.put(name, sharedPreferences);
         }
-        sharedPreferences.edit().putBoolean(key, value).apply();
+        SharedPreferences.Editor editor = sharedPreferences.edit().putBoolean(key, value);
+        SharedPreferencesCompat.apply(editor);
     }
 
+
+    public void clear(String fileName) {
+        SharedPreferences sharedPreferences = mPreferencesMap.get(fileName);
+        if (sharedPreferences != null) {
+            SharedPreferences.Editor clear = sharedPreferences.edit().clear();
+            SharedPreferencesCompat.apply(clear);
+        }
+    }
+
+    public boolean contains(String fileName, String key) {
+        SharedPreferences sharedPreferences = mPreferencesMap.get(fileName);
+        if (sharedPreferences != null) {
+            return sharedPreferences.contains(key);
+        }
+        return false;
+    }
+
+
+    /**
+     * 版本兼容类
+     */
+    static class SharedPreferencesCompat {
+
+        private static Method sMethod = findApplyMethod();
+
+        /**
+         * 查找apply方法
+         *
+         * @return
+         */
+        private static Method findApplyMethod() {
+            try {
+                Class<SharedPreferences.Editor> editorClass = SharedPreferences.Editor.class;
+                return editorClass.getMethod("apply");
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        /**
+         * 早期版本没有apply方法
+         *
+         * @param editor
+         */
+        public static void apply(SharedPreferences.Editor editor) {
+            try {
+                if (sMethod != null) {
+                    sMethod.invoke(editor);
+                    return;
+                }
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+            editor.commit();
+        }
+    }
 }
